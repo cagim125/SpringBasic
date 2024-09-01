6c07edf0c1cfcdc8c8d78dc871414ba5245f8871
cagim125 <cagim30@gmail.com>
2024-09-01 13:48:52 +0900

    User repo, mapper 추가

---
+++ src/main/java/com/oh/demo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.oh.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oh.demo.model.User;
+
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+  public Optional<User> findByEmail(String username);
+  
+}
