8bb3f327b3693a5c017b0404177a1b2ded0d8475
cagim125 <cagim30@gmail.com>
2024-09-01 13:29:56 +0900

    User 초기 데이터 추가

--- src/main/java/com/oh/demo/DemoApplication.java
+++ src/main/java/com/oh/demo/DemoApplication.java
@@ -7,9 +7,12 @@ import java.util.UUID;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.oh.demo.model.Post;
+import com.oh.demo.model.User;
 import com.oh.demo.repository.PostRepository;
+import com.oh.demo.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 public class DemoApplication implements CommandLineRunner {
 	 
 	private final PostRepository postRepository;
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -30,8 +35,19 @@ public class DemoApplication implements CommandLineRunner {
       new Post(UUID.randomUUID(), "title2", "content2", "", LocalDateTime.now()),
       new Post(UUID.randomUUID(), "title3", "content3", "", LocalDateTime.now())
     );
+		for (Post p : postList) {
+			postRepository.save(p);
+		}
 
-		postRepository.saveAll(postList);
+		List<User> userList = List.of(
+			User.builder().email("matthew@test.com")
+					.password(passwordEncoder.encode("1234"))
+					.name("matthew")
+			.build()
+		);
+
+		userRepository.saveAll(userList);
+		
 		
 	}
 
